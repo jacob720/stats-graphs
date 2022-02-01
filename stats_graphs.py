d7634fc89bfd85d4d2d1c6c164fe71ba7d2abdb6
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2022-02-01 17:06:18 +0000

    Improving figures created.

--- stats_graphs.py
+++ stats_graphs.py
@@ -1,29 +1,104 @@
 import numpy as np
-import h5py
+import h5py as h5
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.plotting import table
 import os
 
 default_path = "stats.h5"
 pattern_dict = {"projection": ["SINOGRAM", "PROJECTION", "TANGENTOGRAM", "4D_SCAN", "SINOMOVIE"],
                 "reconstruction": ["VOLUME_YZ", "VOLUME_XZ", "VOLUME_XY", "VOLUME_3D"]}
-_key_list = ["max", "min", "mean", "mean_std_dev", "median_std_dev", "RMSD"]
-
-def load_file(file_name=default_path):
-    return h5py.File(file_name, 'r')
+_stats_list = ["max", "min", "mean", "mean_std_dev", "median_std_dev", "RMSD"]
 
 
-def get_dicts(file):
+def generate_figures(filepath=default_path):
+    f = h5.File(filepath, 'r')
+    stats_dict, index_dict = get_dicts_for_graphs(f)
+    f.close()
+    p_stats = pd.DataFrame(stats_dict["projection"], index_dict["projection"]["max"])
+    r_stats = pd.DataFrame(stats_dict["reconstruction"], index_dict["reconstruction"]["max"])
+    all_stats = pd.concat([p_stats, r_stats], keys=["Projection", "Reconstruction"])
+    all_stats.to_html("stats_table.html")  # create table of all stats for all plugins
+
+    stats_dict, array_plugins = remove_arrays(stats_dict, index_dict)
+
+    p_stats_new = pd.DataFrame(stats_dict["projection"], index_dict["projection"]["max"])
+    r_stats_new = pd.DataFrame(stats_dict["reconstruction"], index_dict["reconstruction"]["max"])
+
+    colours = ["red", "blue", "green", "black", "purple", "brown"]
+    # creating projection stats figure
+    new_p_index = []
+    p_legend = ""
+    for ind in p_stats_new.index:
+        new_p_index.append(ind[0])  # change x ticks to only be plugin numbers rather than names (for space)
+        p_legend += f"{ind}\n"
+    p_stats_new.index = new_p_index
+    fig, ax = plt.subplots(3, 2, figsize=(11, 9), dpi=320, facecolor="azure")
+    i = 0
+    for row in ax:
+        for axis in row:
+            stat = _stats_list[i]
+            axis.plot(p_stats_new[stat], "x-", color=colours[i])
+            for plugin in array_plugins["projection"]:  # adding 'error' bars for plugins with different values due to parameter changes
+                my_max = max(p_stats[stat][plugin])
+                my_min = min(p_stats[stat][plugin])
+                middle = (my_max + my_min)/2
+                my_range = my_max - my_min
+                axis.errorbar(int(plugin[0]) - len(p_stats_new) + 1, middle, yerr=[my_range], capsize=5)
+            if i == 1:
+                maxx = len(p_stats_new[stat])*1.08 - 1
+                maxy = max(p_stats_new[stat])
+                axis.text(maxx, maxy, p_legend, ha="left", va="top", bbox=dict(boxstyle="round", facecolor="red", alpha=0.4))
+            stat.replace("_", " ")
+            axis.set_title(stat)
+            axis.grid(True)
+            i += 1
+    fig.suptitle("Projection Stats", fontsize="x-large")
+    plt.savefig("projection_stats.png", bbox_inches="tight")
+
+    # creating reconstruction stats figure
+    new_r_index = []
+    r_legend = ""
+    for ind in r_stats_new.index:
+        new_r_index.append(ind[0])  # change x ticks to only be plugin numbers rather than names (for space)
+        r_legend += f"{ind}\n"
+    r_stats_new.index = new_r_index
+
+    fig, ax = plt.subplots(3, 2, figsize=(11, 9), dpi=320, facecolor="lavender")
+    i = 0
+    for row in ax:
+        for axis in row:
+            stat = _stats_list[i]
+            axis.plot(r_stats_new[stat], "x-", color=colours[i])
+            for plugin in array_plugins["reconstruction"]:  # adding 'error' bars for plugins with different values due to parameter changes
+                my_max = max(r_stats[stat][plugin])
+                my_min = min(r_stats[stat][plugin])
+                middle = (my_max + my_min)/2
+                my_range = my_max - my_min
+                axis.errorbar(int(plugin[0]) - len(r_stats_new) + 1, middle, yerr=[my_range], capsize=5)
+            if i == 1:
+                maxx = len(r_stats_new[stat])*1.08 - 1
+                maxy = max(r_stats_new[stat])
+                axis.text(maxx, maxy, r_legend, ha="left", va="top", bbox=dict(boxstyle="round", facecolor="red", alpha=0.4))
+            stat = stat.replace("_", " ")
+            axis.set_title(stat)
+            axis.grid(True)
+            i += 1
+    fig.suptitle("Reconstruction Stats", fontsize="x-large")
+    plt.savefig("reconstruction_stats.png", bbox_inches="tight")
+
+
+def get_dicts_for_graphs(file):
     stats_dict = {}
     index_dict = {}
-    stats_dict["projection"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [], "median_std_dev": [], "RMSD": []}
-    stats_dict["reconstruction"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [], "median_std_dev": [],
-                                    "RMSD": []}
-    index_dict["projection"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [], "median_std_dev": [], "RMSD": []}
-    index_dict["reconstruction"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [], "median_std_dev": [],
-                                    "RMSD": []}
+    stats_dict["projection"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [],
+                                "median_std_dev": [], "RMSD": []}
+    stats_dict["reconstruction"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [],
+                                    "median_std_dev": [], "RMSD": []}
+    index_dict["projection"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [],
+                                "median_std_dev": [], "RMSD": []}
+    index_dict["reconstruction"] = {"max": [], "min": [], "mean": [], "mean_std_dev": [],
+                                    "median_std_dev": [], "RMSD": []}
 
     group = file["stats"]
     for space in ("projection", "reconstruction"):
@@ -44,59 +119,15 @@ def get_dicts(file):
     return stats_dict, index_dict
 
 
-def remove_arrays(stats_dict):
+def remove_arrays(stats_dict, index_dict):
+    array_plugins = {"projection": set(()), "reconstruction": set(())}
     for space in list(stats_dict.keys()):
         for stat in list(stats_dict[space].keys()):
             for index, value in enumerate(stats_dict[space][stat]):
                 if isinstance(value, np.ndarray):
                     stats_dict[space][stat][index] = stats_dict[space][stat][index][0]
-    return stats_dict
+                    array_plugins[space].add(index_dict[space][stat][index])
+    return stats_dict, array_plugins
 
 
-def generate_stats(filepath=default_path):
-    f = load_file(filepath)
-    stats_dict, index_dict = get_dicts(f)
-    p_stats = pd.DataFrame(stats_dict["projection"], index_dict["projection"]["max"])
-    r_stats = pd.DataFrame(stats_dict["reconstruction"], index_dict["reconstruction"]["max"])
-    all_stats = pd.concat([p_stats, r_stats], keys=["Projection", "Reconstruction"])
-
-    stats_dict = remove_arrays(stats_dict)
-
-    p_stats = pd.DataFrame(stats_dict["projection"], index_dict["projection"]["max"])
-    r_stats = pd.DataFrame(stats_dict["reconstruction"], index_dict["reconstruction"]["max"])
-
-    kwds = {"marker": "x"}
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-
-    ax1.plot(p_stats, "x-")
-    ax2.plot(r_stats, "x-")
-    ax1.set_title("Projection Stats")
-    ax2.set_title("Reconstruction Stats")
-    fig.autofmt_xdate()
-    for ax in (ax1, ax2):
-        ax.grid(True)
-    ax2.legend(_key_list, loc="right", bbox_to_anchor=(1.8, 0.5))
-
-    plt.plot(grid=True, title="Projection Space Stats", **kwds)
-#    r_stats.plot(ax=grid=True, title="Reconstruction Space Stats", **kwds)
-    #plt.legend(loc="right", bbox_to_anchor=(1.35, 0.5))
-    plt.savefig("stats.png", bbox_inches="tight")
-    #r_stats.plot(grid=True, title="Reconstruction Space Stats", **kwds)
-    #plt.legend(loc="right", bbox_to_anchor=(1.35, 0.5))
-    #plt.savefig("reconstruction_stats.png", bbox_inches="tight")
-
-    all_stats.to_html("stats_table.html")
-
-    #all_stats = all_stats.reset_index()
-
-    #ax = plt.subplot(111, frame_on=False)
-    #ax.xaxis.set_visible(False)  # hide the x axis
-    #ax.yaxis.set_visible(False)  # hide the y axis
-    #t = table(ax, all_stats, loc="center")
-    #plt.savefig("stats_table.png")
-    #excel_data = pd.ExcelWriter("stats_table.xlsx")
-    #all_stats.to_excel(excel_data)
-    #excel_data.save()
-
-generate_stats()
+generate_figures()
